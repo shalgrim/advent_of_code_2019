9d560a0b4bdd502b4db4e92aa4136ae4b1ede109
Scott Halgrim <shalgrim@gmail.com>
2019-12-01 21:28:04 -0800

    day 2 part 1 success

--- day02_1.py
+++ day02_1.py
@@ -12,3 +12,13 @@ def run_program(list_of_ints):
             output[output[reading+3]] = output[output[reading+1]] * output[output[reading+2]]
         reading += 4
     return output
+
+
+if __name__ == '__main__':
+    with open('data/input02.txt') as f:
+        content = f.read()
+    program_input = [int(x) for x in content.split(',')]
+    program_input[1] = 12
+    program_input[2] = 2
+    output = run_program(program_input)
+    print(output[0])
