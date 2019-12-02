673a010542fe86cbedcd102622bae21b57ac3584
Scott Halgrim <shalgrim@gmail.com>
2019-12-01 21:38:10 -0800

    day 2 part 2 success

---
+++ day02_2.py
@@ -0,0 +1,19 @@
+import sys
+
+from day02_1 import run_program
+
+if __name__ == '__main__':
+    with open('data/input02.txt') as f:
+        content = f.read()
+    program_input = [int(x) for x in content.split(',')]
+
+    for noun in range(100):
+        program_input[1] = noun
+        for verb in range(100):
+            program_input[2] = verb
+            output = run_program(program_input)
+            if output[0] == 19690720:
+                print(f'{noun=}')
+                print(f'{verb=}')
+                print(100 * noun + verb)
+                sys.exit()
