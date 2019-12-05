1f4f2ce61ba2de2ea88e025b95a6ac12b1a9714f
Scott Halgrim <shalgrim@gmail.com>
2019-12-05 00:10:10 -0800

    figured out how to mock/patch input

--- test_day05.py
+++ test_day05.py
@@ -1,5 +1,8 @@
 from unittest import TestCase
-from day05_1 import parse_instruction, get_params, process_instruction, run_program
+from unittest.mock import patch
+
+from day05_1 import (get_params, parse_instruction, process_instruction,
+                     run_program)
 
 
 class TestDay05(TestCase):
@@ -14,15 +17,15 @@ class TestDay05(TestCase):
         self.assertEqual(process_instruction(2, [33, 3, 4], output, 0, []), 4)
         self.assertEqual(output, [1002, 4, 3, 4, 99])
 
-    # def test_run_program_part_1(self):
-    #     """Only works if we mock out the input for opcode 3"""
-    #     with open('data/input05_github_login.txt') as f:
-    #         content = f.read()
-    #     program_input = [int(x) for x in content.split(',')]
-    #     the_outputs = run_program(program_input)
-    #     self.assertEqual(the_outputs[-1], 9431221)
+    @patch('builtins.input', lambda x: 1)
+    def test_run_program_part_1(self):
+        """Only works if we mock out the input for opcode 3"""
+        with open('data/input05_github_login.txt') as f:
+            content = f.read()
+        program_input = [int(x) for x in content.split(',')]
+        the_outputs = run_program(program_input)
+        self.assertEqual(the_outputs[-1], 9431221)
 
     # I have to learn how to mock out input
     # def test_run_program_part_2(self):
     #     the_outputs = run_program()
-
