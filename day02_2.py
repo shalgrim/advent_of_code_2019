684780d39bcf80431826bb66e9c6817d73c0e2f5
Scott Halgrim <shalgrim@gmail.com>
2019-12-06 11:42:15 -0800

    new black yay

--- day02_2.py
+++ day02_2.py
@@ -2,10 +2,10 @@ import sys
 
 from day02_1 import run_program
 
-if __name__ == "__main__":
-    with open("data/input02.txt") as f:
+if __name__ == '__main__':
+    with open('data/input02.txt') as f:
         content = f.read()
-    program_input = [int(x) for x in content.split(",")]
+    program_input = [int(x) for x in content.split(',')]
 
     for noun in range(100):
         program_input[1] = noun
@@ -13,7 +13,7 @@ if __name__ == "__main__":
             program_input[2] = verb
             output = run_program(program_input)
             if output[0] == 19690720:
-                print(f"{noun=}")
-                print(f"{verb=}")
+                print(f'{noun=}')
+                print(f'{verb=}')
                 print(100 * noun + verb)
                 sys.exit()
