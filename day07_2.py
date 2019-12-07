0bfd4b27f4a50093478a125d4d0a215a0ceafef4
Scott Halgrim <shalgrim@gmail.com>
2019-12-06 23:41:55 -0800

    day 7 part 2 success

---
+++ day07_2.py
@@ -0,0 +1,87 @@
+import itertools
+from copy import copy
+
+from day05_1 import get_params, parse_instruction
+
+global_output_val = None
+
+
+class Amplifier(object):
+    def __init__(self, instructions, phase):
+        self.memory = copy(instructions)
+        self.phase = phase
+        self.provided_phase = False
+        self.instruction_pointer = 0
+
+    def run_program(self, first_input):
+        global global_output_val
+        while self.memory[self.instruction_pointer] != 99:
+            instruction = self.memory[self.instruction_pointer]
+            opcode, param_modes = parse_instruction(instruction)
+            params = get_params(
+                self.memory, self.instruction_pointer, opcode, param_modes
+            )
+            if opcode == 1:
+                self.memory[params[2]] = params[0] + params[1]
+                self.instruction_pointer += 4
+            elif opcode == 2:
+                self.memory[params[2]] = params[0] * params[1]
+                self.instruction_pointer += 4
+            elif opcode == 3:
+                self.memory[params[0]] = (
+                    first_input if self.provided_phase else self.phase
+                )
+                self.provided_phase = True
+                self.instruction_pointer += 2
+            elif opcode == 4:
+                output_val = self.memory[params[0]]
+                self.instruction_pointer += 2
+                global_output_val = output_val
+                # print(output_val)
+                return output_val
+            elif opcode == 5:
+                if params[0] != 0:
+                    self.instruction_pointer = params[1]
+                else:
+                    self.instruction_pointer += 3
+            elif opcode == 6:
+                if params[0] == 0:
+                    self.instruction_pointer = params[1]
+                else:
+                    self.instruction_pointer += 3
+            elif opcode == 7:
+                self.memory[params[2]] = 1 if params[0] < params[1] else 0
+                self.instruction_pointer += 4
+            elif opcode == 8:
+                self.memory[params[2]] = 1 if params[0] == params[1] else 0
+                self.instruction_pointer += 4
+
+        # print(f'REACHED OPCODE 99 with val {global_output_val}!')
+        return ('Done', global_output_val)
+
+
+def run_amplifier_series(instructions, phases):
+    amplifiers = [Amplifier(instructions, p) for p in phases]
+    amp_index = 0
+    input_val = amplifiers[amp_index].run_program(0)
+    while not (isinstance(input_val, tuple)):
+        amp_index = 0 if amp_index == 4 else amp_index + 1
+        input_val = amplifiers[amp_index].run_program(input_val)
+    return input_val[1]
+
+
+def find_max_signal_in_feedback_mode(instructions):
+    phases = [5, 6, 7, 8, 9]
+    signals = [
+        run_amplifier_series(instructions, list(perm))
+        for perm in itertools.permutations(phases)
+    ]
+    return max(signals)
+
+
+if __name__ == '__main__':
+    with open('data/input07.txt') as f:
+        content = f.read()
+    program_instructions = [int(x) for x in content.split(',')]
+    answer = find_max_signal_in_feedback_mode(program_instructions)
+    print(answer)
--- test_day07.py
+++ test_day07.py
@@ -1,5 +1,7 @@
 from unittest import TestCase
-from day07_1 import run_all_amplifiers, find_max_signal
+
+from day07_1 import find_max_signal, run_all_amplifiers
+from day07_2 import find_max_signal_in_feedback_mode, run_amplifier_series
 
 
 class TestDay07(TestCase):
@@ -8,7 +10,9 @@ class TestDay07(TestCase):
         this_input = [int(x) for x in content.split(',')]
         self.assertEqual(run_all_amplifiers(this_input, [4, 3, 2, 1, 0]), 43210)
         self.assertEqual(find_max_signal(this_input), 43210)
-        content = '3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0'
+        content = (
+            '3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0'
+        )
         this_input = [int(x) for x in content.split(',')]
         self.assertEqual(run_all_amplifiers(this_input, [0, 1, 2, 3, 4]), 54321)
         self.assertEqual(find_max_signal(this_input), 54321)
@@ -19,3 +23,35 @@ class TestDay07(TestCase):
         this_input = [int(x) for x in content.split(',')]
         self.assertEqual(run_all_amplifiers(this_input, [1, 0, 4, 3, 2]), 65210)
         self.assertEqual(find_max_signal(this_input), 65210)
+
+    def test_run_amplifier_series(self):
+        content = (
+            '3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,'
+            '27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5'
+        )
+        instructions = [int(x) for x in content.split(',')]
+        phases = [9, 8, 7, 6, 5]
+        self.assertEqual(run_amplifier_series(instructions, phases), 139629729)
+        content = (  # actually is 18216
+            '3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,'
+            '-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,'
+            '53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10'
+        )
+        instructions = [int(x) for x in content.split(',')]
+        phases = [9, 7, 8, 5, 6]
+        self.assertEqual(run_amplifier_series(instructions, phases), 18216)
+
+    def test_run_find_max_signal_in_feedback_mode(self):
+        content = (
+            '3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,'
+            '27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5'
+        )
+        instructions = [int(x) for x in content.split(',')]
+        self.assertEqual(find_max_signal_in_feedback_mode(instructions), 139629729)
+        content = (  # actually is 18216
+            '3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,'
+            '-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,'
+            '53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10'
+        )
+        instructions = [int(x) for x in content.split(',')]
+        self.assertEqual(find_max_signal_in_feedback_mode(instructions), 18216)
