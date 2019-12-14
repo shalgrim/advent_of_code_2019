5e252bf7b004d88fa8917b19fe45254ac9b6f585
Scott Halgrim <shalgrim@gmail.com>
2019-12-14 02:19:22 -0800

    day 14 part 1 success

---
+++ day14_1.py
@@ -0,0 +1,196 @@
+"""
+Going to bed idea
+Keep track of how much input_ingredient you used, since that's always magically created
+Try to run the rule giving you the output you want with available resources
+If all the ingredients are there in the necessary quantity, run it
+If not, run a rule to produce an ingredient you're short on...note that this will update available resources
+Yep, that's the way to do it
+All input ingredient then is the resource you track
+Other than that it's just: Do I have all the ingredients to run this rule?
+"""
+
+import math
+from copy import copy
+import itertools
+
+
+class Rule(object):
+    def __init__(self, line):
+        lhs, rhs = line.split('=>')
+        lhs = [l.strip().split() for l in lhs.strip().split(',')]
+        self.lhs = {t[1]: int(t[0]) for t in lhs}
+        self.rhs_ingredient = rhs.strip().split()[1]
+        self.rhs_quantity = int(rhs.strip().split()[0])
+
+    @property
+    def output(self):
+        return self.rhs_ingredient
+
+    @property
+    def num_produced(self):
+        return self.rhs_quantity
+
+
+def does_produce(a, b, rules):
+    """True if a produces b"""
+    if b not in rules:
+        return False
+
+    if rules[b].lhs.get(a, False):
+        return True
+    else:
+        return any(does_produce(a, c, rules) for c in rules[b].lhs)
+
+
+num_inputs_produced = 0
+resources = {}
+
+
+def find_missing_ingredient(rule):
+    global resources
+    lhs = rule.lhs
+    for ingredient, required in lhs.items():
+        if resources.get(ingredient, 0) < required:
+            return ingredient
+    return None
+
+
+def get_required(input_ingredient, output_ingredient, rules):
+    global num_inputs_produced, resources
+    output_rule = rules[output_ingredient]
+
+    missing_ingredient = find_missing_ingredient(output_rule)
+    while missing_ingredient:
+        if missing_ingredient == input_ingredient:
+            total_needed = output_rule.lhs[missing_ingredient]
+            new_inputs_needed = output_rule.lhs[input_ingredient] - resources.get(input_ingredient, 0)
+            resources[input_ingredient] = total_needed
+            num_inputs_produced += new_inputs_needed
+        else:
+            get_required(input_ingredient, missing_ingredient, rules)
+
+        missing_ingredient = find_missing_ingredient(output_rule)
+
+    try:
+        resources[output_ingredient] += output_rule.num_produced
+    except KeyError:
+        resources[output_ingredient] = output_rule.num_produced
+    for ing, num in output_rule.lhs.items():
+        resources[ing] -= num
+
+
+# def get_required(input_ingredient, output_ingredient, num_needed, rules):
+#     input_production_rules = [v for k, v in rules.items() if input_ingredient in v.lhs]
+#
+#     direct_rules = [
+#         ipr for ipr in input_production_rules if ipr.output == output_ingredient
+#     ]
+#     if direct_rules:
+#         rule = direct_rules[0]
+#         return rule.lhs[input_ingredient] * math.ceil(num_needed / rule.num_produced)
+#
+#     # if any(ipr.output == output_ingredient for ipr in input_production_rules):
+#     #     rule = [ipr for ipr in input_production_rules if ]
+#
+#     answer = 0
+#     for ipr in input_production_rules:
+#         num_times_to_run_rule = math.ceil(
+#             get_required(ipr.output, output_ingredient, num_needed, rules)
+#             / ipr.num_produced
+#         )
+#         answer += ipr.lhs[input_ingredient] * num_times_to_run_rule
+#     return answer
+#
+
+# def get_required(input_ingredient, output_ingredient, num_needed, rules):
+#     # next try reducing as much as possible
+#     if input_ingredient == output_ingredient:
+#         return num_needed
+#
+#     rule = rules[output_ingredient]
+#
+#     if input_ingredient in rule.lhs:
+#         return rule.lhs[input_ingredient] * math.ceil(num_needed / rule.num_produced)
+#
+#     # assume two input ingredients
+#     ingredient_item_list = list(rule.lhs.items())
+#     ing1, needed1 = ingredient_item_list[0]
+#     ing2, needed2 = ingredient_item_list[1]
+#     if does_produce(ing1, ing2, rules):
+#         num_ing1_needed_to_produce_ing2 = get_required(ing1, ing2, needed2, rules)
+#         total_ing1_needed = needed1 + num_ing1_needed_to_produce_ing2
+#         return get_required(
+#             input_ingredient, ing1, total_ing1_needed * math.ceil(num_needed / rule.num_produced), rules
+#         ) + get_required(input_ingredient, ing2, needed2, rules)
+#     elif does_produce(ing2, ing1, rules):
+#         num_ing2_needed_to_produce_ing1 = get_required(ing2, ing1, needed1, rules)
+#         total_ing2_needed = needed2 + num_ing2_needed_to_produce_ing1
+#         return get_required(
+#             input_ingredient, ing2, total_ing2_needed * math.ceil(num_needed / rule.num_produced), rules
+#         )
+#     else:
+#         return get_required(
+#             input_ingredient, ing1, needed1 * math.ceil(num_needed / rule.num_produced), rules
+#         ) + get_required(
+#             input_ingredient, ing2, needed2 * math.ceil(num_needed / rule.num_produced), rules
+#         )
+#
+#     # intermediate_ingredients = copy(rule.lhs)
+#     # for perm in itertools.permutations(rule.lhs.items(), 2):
+#     #     ing_a, needed_a = perm[0]
+#     #     ing_b, needed_b = perm[1]
+#     #     if does_produce(ing_a, ing_b, rules):
+#     #         intermediate_ingredients[ing_a] += get_required(ing_a, ing_b, math.ceil(num_needed / needed_b), rules)
+#     #         del intermediate_ingredients[ing_b]
+#     #
+#     # answer = 0
+#     # for ii, nn in intermediate_ingredients.items():
+#     #     answer += get_required(ii, output_ingredient, math.ceil(num_needed / nn), rules)
+#     #
+#     # return answer
+#
+
+# def get_required(input_ingredient, output_ingredient, num_needed, rules):
+#     if input_ingredient == output_ingredient:
+#         return num_needed
+#
+#     try:
+#         rule = rules[output_ingredient]
+#     except KeyError:
+#         return 0
+#
+#     # if input_ingredient in rule.lhs:
+#     #     times_to_run_rule = math.ceil(num_needed / rule.num_produced)
+#     #     input_needed_per_run = rule.lhs[input_ingredient]
+#     #     answer = times_to_run_rule * input_needed_per_run
+#     #     TODO: recurse
+#     #
+#     # else:
+#
+#     answer = 0
+#
+#     for ingredient, quantity in rule.lhs.items():
+#         answer += get_required(
+#             input_ingredient,
+#             ingredient,
+#             quantity * math.ceil(num_needed / rule.num_produced),
+#             rules,
+#         )
+#
+#     return answer
+
+def main(lines, input_ingredient='ORE', outupt_ingredient = 'FUEL'):
+    global num_inputs_produced, resources
+    resources = {}
+    num_inputs_produced = 0
+    rules = [Rule(line) for line in lines]
+    rules = {rule.output: rule for rule in rules}
+    get_required('ORE', 'FUEL',  rules)
+    return num_inputs_produced
+
+
+if __name__ == '__main__':
+    with open('data/input14.txt') as f:
+        lines = [line.strip() for line in f.readlines()]
+
+    print(main(lines))
---
+++ test_day14.py
@@ -0,0 +1,101 @@
+from unittest import TestCase
+
+from day14_1 import Rule, main, get_required, does_produce
+
+
+class TestDay14(TestCase):
+    def test_does_produce(self):
+        with open('data/test14_1.txt') as f:
+            lines = [line.strip() for line in f.readlines()]
+        rules = [Rule(line) for line in lines]
+        rules = {rule.output: rule for rule in rules}
+        self.assertFalse(does_produce('A', 'ORE', rules))
+        self.assertFalse(does_produce('B', 'ORE', rules))
+        self.assertFalse(does_produce('C', 'ORE', rules))
+        self.assertFalse(does_produce('D', 'ORE', rules))
+        self.assertFalse(does_produce('E', 'ORE', rules))
+        self.assertFalse(does_produce('FUEL', 'ORE', rules))
+        self.assertTrue(does_produce('ORE', 'A', rules))
+        self.assertTrue(does_produce('ORE', 'B', rules))
+        self.assertTrue(does_produce('ORE', 'C', rules))
+        self.assertTrue(does_produce('ORE', 'D', rules))
+        self.assertTrue(does_produce('ORE', 'E', rules))
+        self.assertTrue(does_produce('ORE', 'FUEL', rules))
+        self.assertFalse(does_produce('A', 'B', rules))
+        self.assertFalse(does_produce('B', 'A', rules))
+        self.assertTrue(does_produce('A', 'C', rules))
+        self.assertTrue(does_produce('A', 'D', rules))
+        self.assertTrue(does_produce('A', 'E', rules))
+        self.assertTrue(does_produce('A', 'FUEL', rules))
+        self.assertFalse(does_produce('C', 'A', rules))
+        self.assertFalse(does_produce('D', 'A', rules))
+        self.assertFalse(does_produce('E', 'A', rules))
+        self.assertFalse(does_produce('FUEL', 'A', rules))
+        self.assertTrue(does_produce('B', 'C', rules))
+        self.assertFalse(does_produce('C', 'B', rules))
+        self.assertTrue(does_produce('B', 'D', rules))
+        self.assertTrue(does_produce('B', 'E', rules))
+        self.assertTrue(does_produce('B', 'FUEL', rules))
+
+    # def test_get_required(self):
+    #     with open('data/test14_1.txt') as f:
+    #         lines = [line.strip() for line in f.readlines()]
+    #     rules = [Rule(line) for line in lines]
+    #     rules = {rule.output: rule for rule in rules}
+    #
+    #     # get tests to pass in this order
+    #     self.assertEqual(get_required('E', 'FUEL', 1, rules), 1)
+    #     self.assertEqual(get_required('D', 'E', 1, rules), 1)
+    #     self.assertEqual(get_required('C', 'D', 1, rules), 1)
+    #     self.assertEqual(get_required('B', 'C', 1, rules), 1)
+    #     self.assertEqual(get_required('B', 'D', 1, rules), 1)
+    #     self.assertEqual(get_required('C', 'E', 1, rules), 1)
+    #     self.assertEqual(get_required('B', 'E', 1, rules), 1)
+    #     self.assertEqual(get_required('D', 'FUEL', 1, rules), 1)
+    #     self.assertEqual(get_required('C', 'FUEL', 1, rules), 1)
+    #     self.assertEqual(get_required('B', 'FUEL', 1, rules), 1)
+    #     self.assertEqual(get_required('ORE', 'B', 1, rules), 1)
+    #     self.assertEqual(get_required('ORE', 'A', 1, rules), 10)
+    #     self.assertEqual(get_required('ORE', 'A', 10, rules), 10)
+    #     self.assertEqual(get_required('ORE', 'A', 11, rules), 20)
+    #     self.assertEqual(get_required('ORE', 'C', 1, rules), 11)
+    #     self.assertEqual(get_required('ORE', 'D', 1, rules), 21)
+    #     self.assertEqual(get_required('ORE', 'E', 1, rules), 31)
+    #     self.assertEqual(get_required('A', 'FUEL', 1, rules), 28)
+    #     self.assertEqual(get_required('ORE', 'FUEL', 1, rules), 31)
+    #
+    #     # should be good if I can get above to pass
+    #     # self.assertEqual(get_required('E', 'FUEL', 1, rules), 1)
+    #     # self.assertEqual(get_required('ORE', 'A', 10, rules), 10)
+    #     # self.assertEqual(get_required('ORE', 'B', 1, rules), 1)
+    #     # self.assertEqual(get_required('ORE', 'C', 1, rules), 11)
+    #     # self.assertEqual(get_required('A', 'C', 1, rules), 7)
+    #     # self.assertEqual(get_required('ORE', 'D', 1, rules), 21)
+    #     # self.assertEqual(get_required('A', 'D', 1, rules), 14)
+    #     # self.assertEqual(get_required('A', 'FUEL', 1, rules), 28)
+    #     # self.assertEqual(get_required('ORE', 'A', 7, rules), 10)
+    #     # self.assertEqual(get_required('ORE', 'A', 14, rules), 20)
+    #     # self.assertEqual(get_required('ORE', 'A', 21, rules), 30)
+    #     # self.assertEqual(get_required('ORE', 'A', 28, rules), 30)
+    #     # self.assertEqual(get_required('A', 'C', 1, rules), 7)
+    #     # self.assertEqual(get_required('A', 'D', 1, rules), 14)
+    #     # self.assertEqual(get_required('A', 'E', 1, rules), 21)
+    #     # self.assertEqual(get_required('ORE', 'E', 1, rules), 31)
+    #     # self.assertEqual(get_required('ORE', 'FUEL', 1, rules), 31)
+
+    def test_main_part_1(self):
+        with open('data/test14_1.txt') as f:
+            lines = [line.strip() for line in f.readlines()]
+        self.assertEqual(main(lines), 31)
+        with open('data/test14_2.txt') as f:
+            lines = [line.strip() for line in f.readlines()]
+        self.assertEqual(main(lines), 165)
+        with open('data/test14_3.txt') as f:
+            lines = [line.strip() for line in f.readlines()]
+        self.assertEqual(main(lines), 13312)
+        with open('data/test14_4.txt') as f:
+            lines = [line.strip() for line in f.readlines()]
+        self.assertEqual(main(lines), 180697)
+        with open('data/test14_5.txt') as f:
+            lines = [line.strip() for line in f.readlines()]
+        self.assertEqual(main(lines), 2210736)
