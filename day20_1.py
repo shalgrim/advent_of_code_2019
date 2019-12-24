b9173ed1a89ae7cc60f369baf1a691c006ec35a5
Scott Halgrim <shalgrim@gmail.com>
2019-12-24 10:42:55 -0800

    day 20 part 1 success

--- day20_1.py
+++ day20_1.py
@@ -1,54 +1,130 @@
+import math
 import re
 from collections import defaultdict
 
 
 class Map(object):
     def __init__(self, lines):
-        self.portals = Map.find_portals(lines)
+        self.portals = defaultdict(list)
+        self.walls = set()
+        self.passages = set()
+        self.build_map(lines)
         self.start = self.portals['AA'][0]
         self.end = self.portals['ZZ'][0]
+        self.distances = defaultdict(lambda: math.inf)
+        self.portal_travel = {}
 
-    @staticmethod
-    def find_portals(lines):
+        for k, v in self.portals.items():
+            if k in ('AA', 'ZZ'):
+                continue
+            self.portal_travel[v[0]] = v[1]
+            self.portal_travel[v[1]] = v[0]
+
+    def calc_all_distances(self, location=None, current_distance=0):
+        if location is None:
+            location = self.start
+
+        if (
+            self.distances[location] <= current_distance
+        ):  # I've already been here and at least as short
+            return
+        self.distances[location] = current_distance
+
+        north = location[0], location[1] - 1
+        south = location[0], location[1] + 1
+        east = location[0] + 1, location[1]
+        west = location[0] - 1, location[1]
+        # portal = ... figure this out
+        directions = [north, south, east, west]  # add portal
+
+        for direction in [d for d in directions if d in self.passages]:
+            self.calc_all_distances(direction, current_distance + 1)
+
+        if location in self.portal_travel:
+            self.calc_all_distances(self.portal_travel[location], current_distance + 1)
+
+    def display(self):
+        lines = []
+        max_x = max(w[0] for w in self.walls)
+        max_y = max(w[1] for w in self.walls)
+
+        for y in range(max_y+1):
+            line = []
+            for x in range(max_x+1):
+                if (x, y) in self.walls:
+                    line.append('#')
+                elif (x, y) in self.distances:
+                    val = self.distances[(x,y)]
+                    if val < 10:
+                        line.append(str(val))
+                    elif val % 10 == 0:
+                        line.append(str(val // 10))
+                    else:
+                        line.append(str(val % 10))
+                else:
+                    line.append(' ')
+            lines.append(''.join(line))
+        for line in lines:
+            print(line)
+
+    def build_map(self, lines):
         horizontal_label = re.compile('[A-Z]{2}')
         vertical_label = re.compile('(?<![A-Z])[A-Z](?![A-Z])')
 
-        portals = defaultdict(list)
-
         for y, line in enumerate(lines[:-1]):
+            for x, c in enumerate(line):
+                if c == '#':
+                    self.walls.add((x, y))
+                elif c == '.':
+                    self.passages.add((x, y))
             for hl in horizontal_label.findall(line):
                 label_start = line.find(hl)
                 if label_start == 0:
-                    portals[hl].append((2, y))
+                    self.portals[hl].append((2, y))
                 elif label_start == len(line) - 2:
-                    portals[hl].append((len(line) - 2, y))
+                    self.portals[hl].append((len(line) - 3, y))
                 else:
-                    if line[label_start-1] == '.':
-                        portals[hl].append((label_start-1, y))
+                    if line[label_start - 1] == '.':
+                        self.portals[hl].append((label_start - 1, y))
                     else:
-                        portals[hl].append((label_start+2, y))
+                        self.portals[hl].append((label_start + 2, y))
 
-            vertical_labels = vertical_label.findall(line)
-            # vertical_labels = [vl for vl in vertical_labels if line.find(vl) not in (0, len(line)-1)]
-            vertical_labels = [(m.group(), m.start()) for m in vertical_label.finditer(line)]
-            assert len(vertical_labels) == len(set([vl[0] for vl in vertical_labels])), 'This will be trickier'
+            vertical_labels = [
+                (m.group(), m.start()) for m in vertical_label.finditer(line)
+            ]
             for txt, x in vertical_labels:
                 if y == 0:
-                    portals[txt + lines[y+1][x]].append((x, 2))
-                elif ord('A') <= ord(lines[y-1][x]) <= ord('Z'):  # processed last time through
+                    self.portals[txt + lines[y + 1][x]].append((x, 2))
+                elif (
+                    ord('A') <= ord(lines[y - 1][x]) <= ord('Z')
+                ):  # processed last time through
                     pass
                 else:
                     if y == len(lines) - 2:
                         portal_y = len(lines) - 3
-                        portals[txt + lines[y+1][x]].append((x, y-1))
-                    elif lines[y+2][x] == '.':
-                        portals[txt + lines[y+1][x]].append((x, y+2))
+                        self.portals[txt + lines[y + 1][x]].append((x, y - 1))
+                    elif lines[y + 2][x] == '.':
+                        self.portals[txt + lines[y + 1][x]].append((x, y + 2))
                     else:
-                        portals[txt + lines[y+1][x]].append((x, y-1))
-
-        return portals
+                        self.portals[txt + lines[y + 1][x]].append((x, y - 1))
 
 
 if __name__ == '__main__':
     with open('data/test20_1.txt') as f:
         lines = [line.rstrip() for line in f.readlines()]
+    map = Map(lines)
+    map.calc_all_distances()
+    map.display()
+    with open('data/test20_2.txt') as f:
+        lines = [line.rstrip() for line in f.readlines()]
+    map = Map(lines)
+    map.calc_all_distances()
+    map.display()
+    with open('data/input20.txt') as f:
+        lines = [line.rstrip() for line in f.readlines()]
+    map = Map(lines)
+    try:
+        map.calc_all_distances()  # max recursion depth?
+    except Exception:
+        pass
+    print(map.distances[map.end])  # 580 is correct
--- test_day20.py
+++ test_day20.py
@@ -7,12 +7,24 @@ class TestDay20(TestCase):
         with open('data/test20_1.txt') as f:
             lines = [line.rstrip() for line in f.readlines()]
         self.map1 = Map(lines)
+        with open('data/test20_2.txt') as f:
+            lines = [line.rstrip() for line in f.readlines()]
+        self.map2 = Map(lines)
+        with open('data/input20.txt') as f:
+            lines = [line.rstrip() for line in f.readlines()]
+        self.map3 = Map(lines)
+        self.map1.calc_all_distances()
+        self.map2.calc_all_distances()
 
     def test_find_start(self):
         self.assertEqual(self.map1.start, (9, 2))
+        self.assertEqual(self.map2.start, (19, 2))
+        self.assertEqual(self.map3.start, (61, 2))
 
     def test_find_end(self):
         self.assertEqual(self.map1.end, (13, 16))
+        self.assertEqual(self.map2.end, (2, 17))
+        self.assertEqual(self.map3.end, (71, 2))
 
     def test_find_portals(self):
         self.assertEqual(len(self.map1.portals), 5)
@@ -21,3 +33,14 @@ class TestDay20(TestCase):
         self.assertCountEqual(self.map1.portals['BC'], [(2, 8), (9, 6)])
         self.assertCountEqual(self.map1.portals['DE'], [(6, 10), (2, 13)])
         self.assertCountEqual(self.map1.portals['FG'], [(2, 15), (11, 12)])
+        self.assertEqual(len(self.map2.portals), 12)
+        self.assertEqual(len(self.map2.portals['AA']), 1)
+        self.assertEqual(len(self.map2.portals['ZZ']), 1)
+        self.assertCountEqual(self.map2.portals['JO'], [(2, 19), (13, 28)])
+        self.assertCountEqual(self.map2.portals['JP'], [(21, 28), (15, 34)])
+        self.assertCountEqual(self.map2.portals['VT'], [(32, 11), (26, 23)])
+        self.assertCountEqual(self.map2.portals['AS'], [(17, 8), (32, 17)])
+
+    def test_calc_shortest_path(self):
+        self.assertEqual(self.map1.distances[self.map1.end], 23)
+        self.assertEqual(self.map2.distances[self.map2.end], 58)
